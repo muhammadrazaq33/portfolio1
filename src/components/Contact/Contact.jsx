a0a425912b7ff7957cf865e718b96cc5162785de
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-05 09:49:17 -0700

    email functionality works

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current, {
+        publicKey: "YOUR_PUBLIC_KEY",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          e.target.reset();
+          alert("Email Send!")
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
-    <div id="contact" className='contact'>
+    <div id="contact" onSubmit={sendEmail} className='contact'>
           <h1 className='text-[3rem] font-semibold'>Contact Me</h1>
           <span className='text-xl font-normal'>Please fill out the form below to discuss any work opportunities.</span>
-          <form className='flex flex-col items-center justify-center gap-[1rem] w-[90vw] max-w-[60rem]'>
+          <form ref={form} className='flex flex-col items-center justify-center gap-[1rem] w-[90vw] max-w-[60rem]'>
               <input type="text" className='inputArea' placeholder='Your Name' />
               <input type="email" className='inputArea' placeholder='Your Email' />
               <textarea name="message" className='inputArea' rows="5" placeholder='Your message'></textarea>
