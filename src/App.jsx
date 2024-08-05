f646ed1ea12c5bdbaab2669bc17956ba5004cb90
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-05 00:19:54 -0700

    footer also completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className=''>
       <Navbar />
       <Header />
-      <Contact/>
+      <Contact />
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="w-[100vw] h-[4rem] bg-[#282828] flex justify-center items-center font-normal text-[1.2rem]">
+      Copyright &#169; Muhammad Razaq. All right received.
+    </footer>
+  );
+}
+
+export default Footer
