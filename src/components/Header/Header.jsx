38367b976933d3f56d75f21de49f2b5b05c0d760
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-09-01 23:43:45 +0500

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import "./Header.css"
-import { Link } from 'react-scroll'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-scroll";
 
 const Header = () => {
   return (
-    <section className='header' id='header'> 
-      <div className='h-[100vh] w-[100vw] text-[3rem] font-semibold p-[2rem] flex flex-col justify-center'>
-        <span className='text-[1.75rem] font-light'>Hello,</span>
+    <section className="header mb-20" id="header">
+      <div className="h-[100vh] w-[100vw] text-[3rem] font-semibold p-[2rem] flex flex-col justify-center">
+        <span className="text-[1.75rem] font-light">Hello,</span>
         <span>
-          I'm <span className='text-[yellow]'>Muhammad Razaq</span>
+          I'm <span className="text-[yellow]">Muhammad Razaq</span>
           <br /> Mern Stack Developer
         </span>
-        <p className='text-lg font-light'>I am a skilled web developer with experience in creating usually appealing and user friendly websites.</p>
-        <Link><button className='bg-[white] my-[1rem] py-[0.75rem] px-[1.8rem] rounded-[2rem] text-[black] text-[18px]'>Hire Me</button></Link>
+        <p className="text-lg font-light max-w-[30rem]">
+          I am a skilled web developer with experience in creating usually
+          appealing and user friendly websites.
+        </p>
+        <Link>
+          <button className="bg-[white] my-[1rem] py-[0.75rem] px-[1.8rem] rounded-[2rem] text-[black] text-[18px]">
+            Hire Me
+          </button>
+        </Link>
       </div>
-      <img src="" alt="" />
+      <img src="/assets/my-image.jpg" alt="" />
     </section>
   );
-}
+};
 
-export default Header
+export default Header;
