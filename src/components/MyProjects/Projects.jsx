c53a523c076537ad48f8c8862407f8aa26123421
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-09-29 00:57:07 +0500

    updated

--- src/components/MyProjects/Projects.jsx
+++ src/components/MyProjects/Projects.jsx
@@ -2,19 +2,41 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <section class="w-screen max-w-screen-xl mx-auto px-4">
-      <h2 class="text-4xl w-max mx-auto text-center text-[#1788ae] border-b-2 border-[#1788ae]">
+    <section className="w-screen max-w-screen-xl mx-auto px-4 relative">
+      <h2 className="text-4xl w-max mx-auto text-center text-[#1788ae] border-b-2 border-[#1788ae] relative z-10 bg-[#212121]">
         Latest Works
       </h2>
-      <div class="md:flex gap-[80px] items-center mt-12">
-        <a href="#" class="w-full">
-          <img
-            class="max-w-[400px] mr-auto"
-            src="https://anuragsinghbam.netlify.app/images/harigurus.webp"
-            alt=""
-          />
+      <div className="md:flex gap-[80px] items-center mt-12 relative">
+        <div className="h-[2px] absolute bg-[#1788ae] left-1/4 right-1/2 top-1/2"></div>
+        <a href="#" className="w-full flex">
+          <div className="relative group hover:scale-105 ease-in-out duration-200">
+            <span class="flex items-center gap-1.5 px-3 py-2 bg-[#fc815c] rounded w-max group-hover:-top-14 ease-jump duration-300 sm:absolute left-1/2 sm:-translate-x-1/2 top-5 text-sm sm:text-base mt-2 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+              ProCodrr
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+            <img
+              className="max-w-[400px] mr-auto relative"
+              src="https://anuragsinghbam.netlify.app/images/harigurus.webp"
+              alt=""
+            />
+          </div>
         </a>
-        <div class="w-full">
+        <div className="w-full">
           <h3 class="text-[#fc815c] font-bold text-4xl">Harigurus</h3>
           <span class="text-[#fc815c] text-lg">(Event Booking)</span>
           <p class="text-justify mt-2">
@@ -53,7 +75,7 @@ const Projects = () => {
           </ul>
         </div>
       </div>
-      <div class="md:flex gap-[80px] items-center mt-12">
+      <div className="md:flex gap-[80px] items-center mt-12">
         <div class="w-full">
           <h3 class="text-[#ffe578] font-bold text-4xl">EazyGrad</h3>
           <span class="text-[#ffe578] text-lg">(EdTech Startup)</span>
@@ -93,14 +115,36 @@ const Projects = () => {
             </li>
           </ul>
         </div>
-        <a href="#" class="w-full">
-          <img
-            class="max-w-[400px] ml-auto"
-            src="https://anuragsinghbam.netlify.app/images/eazygrad.webp"
-            alt=""
-          />
+        <a href="#" className="w-full flex">
+          <div className="relative group hover:scale-105 ease-in-out duration-200">
+            <span class="flex items-center gap-1.5 px-3 py-2 bg-[#ffe578] text-black rounded w-max group-hover:-top-14 ease-jump duration-300 sm:absolute left-1/2 sm:-translate-x-1/2 top-5 text-sm sm:text-base mt-2 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+              Eaxy
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+            <img
+              className="max-w-[400px] mr-auto relative"
+              src="https://anuragsinghbam.netlify.app/images/eazygrad.webp"
+              alt=""
+            />
+          </div>
         </a>
       </div>
+      <div className="w-[2px] absolute bottom-0 top-0 bg-[#1788ae] left-1/2 -translate-x-1/2"></div>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        jump: "cubic-bezier(0.68,-0.55,0.265,1.55)",
+      },
+    },
   },
   plugins: [],
-}
+};
