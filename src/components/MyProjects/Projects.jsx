9433579d3c1a759102103fe344e3b885a3969dd4
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-09-29 01:55:33 +0500

    updated

--- src/components/MyProjects/Projects.jsx
+++ src/components/MyProjects/Projects.jsx
@@ -8,6 +8,7 @@ const Projects = () => {
       </h2>
       <div className="md:flex gap-[80px] items-center mt-12 relative">
         <div className="h-[2px] absolute bg-[#1788ae] left-1/4 right-1/2 top-1/2"></div>
+        <div className="w-4 h-4 absolute left-1/2 -translate-x-1/2 border-[3px] border-[#fc815c] z-10 bg-[#212121] cursor-pointer rounded-full hover:scale-110 ease-in-out duration-100"></div>
         <a href="#" className="w-full flex">
           <div className="relative group hover:scale-105 ease-in-out duration-200">
             <span class="flex items-center gap-1.5 px-3 py-2 bg-[#fc815c] rounded w-max group-hover:-top-14 ease-jump duration-300 sm:absolute left-1/2 sm:-translate-x-1/2 top-5 text-sm sm:text-base mt-2 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
@@ -75,7 +76,9 @@ const Projects = () => {
           </ul>
         </div>
       </div>
-      <div className="md:flex gap-[80px] items-center mt-12">
+      <div className="md:flex gap-[80px] items-center mt-12 relative">
+        <div className="h-[2px] absolute bg-[#1788ae] left-1/2 right-1/4 top-1/2"></div>
+        <div className="w-4 h-4 absolute left-1/2 -translate-x-1/2 border-[3px] border-[#fc815c] z-10 bg-[#212121] cursor-pointer rounded-full hover:scale-110 ease-in-out duration-100"></div>
         <div class="w-full">
           <h3 class="text-[#ffe578] font-bold text-4xl">EazyGrad</h3>
           <span class="text-[#ffe578] text-lg">(EdTech Startup)</span>
@@ -116,7 +119,7 @@ const Projects = () => {
           </ul>
         </div>
         <a href="#" className="w-full flex">
-          <div className="relative group hover:scale-105 ease-in-out duration-200">
+          <div className="ml-auto relative group hover:scale-105 ease-in-out duration-200">
             <span class="flex items-center gap-1.5 px-3 py-2 bg-[#ffe578] text-black rounded w-max group-hover:-top-14 ease-jump duration-300 sm:absolute left-1/2 sm:-translate-x-1/2 top-5 text-sm sm:text-base mt-2 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
               Eaxy
               <svg
@@ -137,7 +140,7 @@ const Projects = () => {
               </svg>
             </span>
             <img
-              className="max-w-[400px] mr-auto relative"
+              className="max-w-[400px] relative"
               src="https://anuragsinghbam.netlify.app/images/eazygrad.webp"
               alt=""
             />
